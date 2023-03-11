66f9a9625d39b701888561d022c0ee912f506116
yuvrajSingh <110782305+yuvrajSingh175@users.noreply.github.com>
2023-03-11 18:36:24 +0530

    Add files via upload

---
+++ timeConversion.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+ * Complete the 'timeConversion' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+string timeConversion(string s) {
+    if(s[8]=='A'){
+        if(s[0]=='1' && s[1]=='2'){
+            s[0]=s[1]='0';
+        }
+    }else{
+        if(s[0]=='0' && s[1]=='1'){
+            s[0] = '1';
+            s[1] = '3';
+        }else if(s[0]=='0' && s[1]=='2'){
+            s[0] = '1';
+            s[1] = '4';
+        }else if(s[1]=='3'){
+            s[0] = '1';
+            s[1] = '5';
+        }else if(s[1]=='4'){
+            s[0] = '1';
+            s[1] = '6';
+        }else if(s[1]=='5'){
+            s[0] = '1';
+            s[1] = '7';
+        }else if(s[1]=='6'){
+            s[0] = '1';
+            s[1] = '8';
+        }else if(s[1]=='7'){
+            s[0] = '1';
+            s[1] = '9';
+        }else if(s[1]=='8'){
+            s[0] = '2';
+            s[1] = '0';
+        }else if(s[1]=='9'){
+            s[0] = '2';
+            s[1] = '1';
+        }else if(s[0]=='1' && s[1]=='0'){
+            s[0] = '2';
+            s[1] = '2';
+        }else if(s[0]=='1' && s[1]=='1'){
+            s[0] = '2';
+            s[1] = '3';
+        }else{
+            s[0]='1';
+            s[1]='2';
+        }
+    }
+    s[8]=s[9]=' ';
+    return s;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    string result = timeConversion(s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
